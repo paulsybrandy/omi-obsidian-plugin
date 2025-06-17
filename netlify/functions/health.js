8a3f14f86bf87f54f25009a414fd94b021f1b4aa
Paul Sybrandy <83975391+paulsybrandy@users.noreply.github.com>
2025-06-17 08:28:29 -0400

    Create health.js

---
+++ netlify/functions/health.js
@@ -0,0 +1,14 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      status: 'healthy',
+      service: 'omi-obsidian-plugin',
+      platform: 'netlify'
+    })
+  }
+}
